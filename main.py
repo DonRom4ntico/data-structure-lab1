06b21ab29d0ab1db450e95e0a82769cd0b3c2ade
angelo <angelojamescoronadoasher@gmail.com>
2024-08-22 11:47:47 +0800

    first lab

---
+++ main.py
@@ -0,0 +1,18 @@
+firstNum = input("Enter first number: ")
+secondNum = input("Enter second number")
+
+if firstNum >= secondNum:
+    print(f"{firstNum} is greater than {secondNum}")
+
+elif firstNum <= secondNum:
+    print(f"{firstNum} is less than {secondNum}")
+elif firstNum == secondNum:
+    print(f"{firstNum} is equals to {secondNum}")
+
+
+firstN = input("Enter classmate name 1 : ")
+secondN = input("Enter classmate name 2 : ")
+thirdN = input("Enter classmate name 3 : ")
+
+print(f"Your classmate are: {firstN}, {secondN}, {thirdN}.")
+
